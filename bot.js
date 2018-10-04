46a5934a6b58b56be12adac5c446aa05b7f39d49
AlphaHosting <42865213+AlphaHosting@users.noreply.github.com>
2018-10-04 22:21:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ const queue = new Map();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Hero Host music |*help`,"http://twitch.tv/S-F")
+client.user.setGame(`Hero Host music |.help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('+[-----------------------------------------------------------------]+')
